cc554280d0da4300167bfb79ae32010730cedd99
lucnguyen0611 <ntluc.work@gmail.com>
2025-07-21 23:11:12 +0700

    test

--- Day55-56/api/src/modules/job/entities.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Column } from 'typeorm';
-import {BaseEntity} from "@/modules/base/entities";
-
-@Entity('job')
-export class JobEntity extends BaseEntity {
-  @Column()
-  name: string;
-}
